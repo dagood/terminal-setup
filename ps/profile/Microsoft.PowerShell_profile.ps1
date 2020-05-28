190fa8917e318bd680e99ac992676169bc7b16d4
Davis Goodin <dagood@microsoft.com>
2020-05-27 23:10:59 -0500

    Add "echo $PROFILE" hint comment

--- ps/profile/Microsoft.PowerShell_profile.ps1
+++ ps/profile/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
+# Run 'echo $PROFILE' to find target location for this script.
+# For example: C:\Users\dagood\OneDrive - Microsoft\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
 $psdir="C:\git\terminal-setup\ps\profile\utils"
 $toLoad = Get-ChildItem "${psdir}\*.ps1"
 $toLoadNames = $toLoad | %{ $_.Name }
